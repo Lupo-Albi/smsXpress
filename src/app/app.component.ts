9455345af939dfdbd555420dba1a10ba6d4cfe56
Lupo-Albi <luandersonalbuquerque@gmail.com>
2018-12-19 21:03:27 -0300

    Configurando o componente para a inserção do sms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ApiService } from './api.service';
+import { Router, ActivatedRoute } from '@angular/router'
 
 declare var $: any;
 
@@ -15,14 +16,25 @@ export class AppComponent {
   title = 'smsXpress-angular';
 
   sms: any;
+  // Variável para o Form Group
+  smsForm: FormGroup;
+  // Variáveis para os campos do formulário
+  numeros:string[]=[''];
+  mensagem:string='';
 
+  // Construtor
   constructor(
     private modalService: BsModalService, // serviço do ngx-bootstrap para mostrar o modal
-    private api: ApiService
+    private api: ApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
     ) { } 
 
-  public modalRef: BsModalRef;  // Variável para manter uma referência do modal
-  config = 
+  // Variável para manter uma referência do modal
+  public modalRef: BsModalRef; 
+  // Variável de configuração do modal
+  configModal = 
   {
     backdrop:true,
     ignoreBackdropClick: true // Opção para não dispensar o modal ao clicar numa área fora dele
@@ -30,11 +42,39 @@ export class AppComponent {
 
   public openModal(template: TemplateRef<any>)
   {
-    this.modalRef = this.modalService.show(template, this.config); // Quando o botão é clicado, a referência ao modal é mantida e o nome local ao template é passado para o serviço
+    this.modalRef = this.modalService.show(template, this.configModal); // Quando o botão é clicado, a referência ao modal é mantida e o nome local ao template é passado para o serviço
+  }
+
+  // Função para retornar um registro de SMS pela API
+  getSMS(protocolo)
+  {
+    this.api.getSMS(protocolo)
+      .subscribe(data => {
+        console.log(data);
+        this.sms = data;
+      });
+  }
+
+  // Função para enviar o formulário
+  onFormSubmit(form: NgForm)
+  {
+    this.api.postSMS(form)
+      .subscribe(res => {}, (err) => {
+        console.log(err);
+      })
   }
 
   public ngOnInit()
   {
+    // Chamada da função 'getSMS' ao inicializar o componente
+    this.getSMS(this.route.snapshot.params['protocolo']); 
+
+    this.smsForm = this.formBuilder.group
+    ({
+      'numeros' : [null, Validators.required],
+      'mensagem' : [null, Validators.required]
+    })
+
     // Início dos Scripts do template Startbootstrap-new-age
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
